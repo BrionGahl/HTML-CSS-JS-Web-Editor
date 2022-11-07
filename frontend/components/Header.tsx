d6ce95295204ac52bf01e51ef8fb7b988ca704d3
BrionGahl <BrionGahl@gmail.com>
2022-11-06 20:21:09 -0600

    large amount of work creating dashboard, working create, delete, and goto buttons for dash, and procedural mapping.

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -6,9 +6,8 @@ import Router from 'next/router'
 
 const Header = () => {
     return (
-        <div className={styles.navbar}>
+        <div className={styles.navbar} style={{fontFamily: "tahoma", paddingBottom: "1%"}}>
             <Link href={`/`}><a className={styles.navlinks} >Home</a></Link>
-            <Link href={`/workspace`}><a className={styles.navlinks}>Workspace</a></Link>
             <Link href={`/login`}><a className={styles.navlinks}>Login</a></Link>
             <Link href={`/`}><a className={styles.navlinks} onClick={e => {deleteCookie("user", {"path": "/"}); Router.reload();}}>Logout</a></Link>
         </div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { getCookie, getCookies, hasCookie } from 'cookies-next'
+import Card from '@mui/material/Card'
+import { Button, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
+import axios from 'axios'
+import { API } from '../utils/constants'
+import Router from 'next/router';
+
+
 
 type Props = {
   user?: string,
@@ -12,11 +19,73 @@ const Home: NextPage<Props> = ({user, tkn}) => {
   const [username, setUsername] = useState(user)
   const [token, setToken] = useState(tkn)
 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    if (!username)
+      return
+    
+    let config = {
+      headers: {Authorization: `Bearer ${token}`}
+    }
+
+    axios.get(`${API}/workspace/user/admin`, config).then(response => {
+      setData(response.data)
+    })
+  }, [])
+
+  const createOnClick = () => {
+    let config = {
+      headers: {Authorization: `Bearer ${token}`}
+    }
+    let data = {
+      "html": "",
+      "css": "",
+      "js": "",
+      "username": username
+    }
+
+    axios.post(`${API}/workspace/save`, data, config).then(resp => {
+      Router.push(`/workspace/${resp.data.id}`)
+    })
+  }
+
+  const listWorkspaces = data.map((workspace, index) =>
+    <Card key={index} sx={{maxWidth: 150}} style={{borderStyle: "solid", borderColor: "#B0B0B0", borderWidth:"1px"}}>
+      <CardActionArea>
+        <CardMedia component="img" height="100" image="/htmlcssjs.png" alt="html css js"/>
+        <CardContent>
+          <Typography gutterBottom component="div">
+            Workspace {index}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" variant="contained" color="primary" onClick={() => {Router.push(`/workspace/${data[index]["id"]}`)}}>
+          Goto
+        </Button>
+        <Button size="small" variant="outlined" color="primary" onClick={() => {
+          let config = {
+            headers: {Authorization: `Bearer ${token}`}
+          }
+          axios.post(`${API}/workspace/${data[index]["id"]}`, {}, config)
+          Router.reload()
+        }}>
+          Delete
+        </Button>
+      </CardActions>
+    </Card> 
+  )
+
   return (
-    <div>
-        <Header />
-        <h3>{username ? `Welcome ${username}!` : `Please Login.`}</h3>
-        <h1>CS435 Project</h1>
+    <div style={{fontFamily: "tahoma", fontWeight: "bold"}}>
+      <h1 style={{textAlign: "center"}}>Editor Application</h1>
+      <Header />
+      <h3>{username ? `Welcome ${username}!` : `Please Login.`}</h3>
+      <Button variant="contained" onClick={createOnClick} style={{marginBottom: "1%"}}>Create</Button>
+      <div className="horizontal-container">
+        {listWorkspaces}
+      </div>
     </div>
   )
 }
